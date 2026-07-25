e32db7d6b2bd5ec7b4b3c20a8a51ea647c3fcac2
mabdullah356 <abdullahworld111@gmail.com>
2026-07-25 23:17:45 +0500

    add : create user model with mongoose schema

---
+++ server/src/Models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose  = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+
+    fullName:{
+        type: String,
+        required:[true,"Username is required"],
+        unique:[true , "Username is Unique"]
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:[true , "Email is Unique"]
+
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+    avatar:{
+        type:String,
+        default:"https://images.unsplash.com/photo-1781204515883-9f6863f32c58?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    isOnline:{
+        type:Boolean,
+        default:false
+    },
+    isTyping:{
+        type:Boolean,
+        default:false
+    },
+    socketId:{
+        type:Strung
+    }
+
+},{timestamps:true});
+
+const User = new mongoose.model("User",userSchema);
+
+module.exports = User;
